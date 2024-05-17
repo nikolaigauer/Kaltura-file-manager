696d0accf222e9d35e2ef18c0fd4b7872d827cdf
Nikolai Gauer <nikolaigauer@gmail.com>
2024-05-17 14:57:30 -0700

    fixed error in main.js again

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+require('update-electron-app')()
 const { app, BrowserWindow } = require('electron')
 
 // include the Node.js 'path' module at the top of your file
 const path = require('path')
-const update = require('update-electron-app')()
+
 
 // modify your existing createWindow() function
 const createWindow = () => {
